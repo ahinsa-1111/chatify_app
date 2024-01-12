5cb602564d686477dcaf817850a473a8097364ce
ahinsa-1111 <148263893+ahinsa-1111@users.noreply.github.com>
2024-01-12 13:59:21 +0530

    commit_02

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,11 @@
+import 'package:chatify_app/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+Future<void> main() async {
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
@@ -31,7 +36,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Chatify'),
     );
   }
 }
